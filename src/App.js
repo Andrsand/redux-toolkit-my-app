4c5df04b7eaf12fdc53004344cc326e2ba050e14
andrsand <angorko@list.ru>
2022-11-19 00:33:50 +0300

    Added comments

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
+        <Counter /> {/* добавлен компонент Counter из features/counter/Counter.js */}
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import counterReducer from '../features/counter/counterSlice'; // импорт функции редюсера из слайса счетчика
 
 export const store = configureStore({  // создание пустого хранилища Redux и его экспорт
   reducer: {
-    counter: counterReducer,
+    counter: counterReducer, // поле с параметром  указавающим использовать эту функцию сокращения среза для обработки всех обновлений этого состояния. 
   },
 });
